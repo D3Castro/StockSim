1da328ba9e6754570ec781e9f6f3a1d60f0a4f25
Luis-S <31809470+Luis-S@users.noreply.github.com>
2017-11-19 17:59:56 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,60 +1,57 @@
 import socket
+import time
 import sys
 
 class Client:
     __init__(self,cashAvailable):
-        self.wallet = cashAvailable
-        self.owned  = []
- 
+        self.ballance = cashAvailable
+        self.yielding = None
+        self.purchasedStocks  = []
+        self.favoriteStocks   = []
+    
 class Stack:
     def __init__(self):
         self.list = []
-
     def push(self,item):
         self.list.append(item)
-
     def pop(self):
         return self.list.pop()
-
     def isEmpty(self):
         return len(self.list) == 0
 
 #server should get the first string in each array of strings
-def buy(client,stock,numStocks,price):
-    #AF_INET for ipv4, .6 for ipv6
+def startTransaction(action,client,numStocks,price,serverIP="192.168.1.11",portNum=2044):
     try:
         clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     except socket.error,msg:
         print "failed. error code: "+ str(msg[0])+ " and error msg = "+msg[1]
         sys.exit()
-    serverIP = "192.168.1.11"
-    
-    clientSocket.connect((serverIP,2044))
-    
-    clientSocket.sendall("BUY  "+stock+" "+numStocks+" "+price)    
-    #should receive message saying  buy/sell success
-    recvMsg = clientSocket.recv(4096) 
-    print "recieved message = ",recvMsg
-    clientSocket.close()
-def sell(client,stock,numStocks,price):
-    #AF_INET for ipv4, .6 for ipv6
-    try:
-        clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    except socket.error,msg:
-        print "failed. error code: "+ str(msg[0])+ " and error msg = "+msg[1]
-        sys.exit()
-    serverIP = "192.168.1.11"
-    
-    clientSocket.connect((serverIP,2044))
-    
-    clientSocket.sendall("SELL "+stock+" "+numStocks+" "+price)    
-    #should receive message saying  buy/sell success
-    recvMsg = clientSocket.recv(4096) 
-    if(recvMS == "fail"):
-        print "error occured with transaction"
+        
+    if action == "BUY":
+        #stock randomly selected from list of favorite stocks 
+        stock = None
+    elif action == "SELL":
+        #stock randomly selected from purchased
+        stock = None
     else:
-        print recvMsg
+        pass
+    
+    clientSocket.connect((serverIP,portNum))
+    clientSocket.sendall(action+" "+stock+" "+numStocks+" "+price)    
+    
+    #should receive message saying  buy/sell success 
+    recvMsg = clientSocket.recv(4096) 
+    
+    
+    #Update with correct format
+    print "recieved message = ",recvMsg
+    if(recvMsg == "SUCCESS"):
+        #update stocks
+        time.sleep(1)   #time in seconds
+        
+    
     clientSocket.close()
+
 
 #function to get info from text file and push to a stack
 def getInfo(textFile,stockStack):
@@ -63,8 +60,6 @@ def getInfo(textFile,stockStack):
                 x = line
                 #String formatting done here
                 stockStack.push(x)
-    
-                
     
 if __name__ == "__main__":
     clients = []
@@ -88,6 +83,10 @@ if __name__ == "__main__":
    
             
     
+    
+    #print balance and yielding of each client every 10 seconds
+    #Assume price information available at client side (stack known)
+    #Clients buy/sell and know their balance/yielding
     
     
     
